cd1f4693a35b19d629e3a0de79a1c1cf218be992
elenasanchezp <elena.sanchez.p@outlook.com>
2018-03-11 11:39:30 +0000

    tidy up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,50 @@
         });  
     });
 
+    function addAnimation(element, addClass, distance) {
+		var elemPos;
+
+		$(element).each(function(){
+			elemPos = $(this).offset().top;
+			
+			if(!distance) {
+				distance = 1.05;
+			}
+
+			var enterPos = $(window).scrollTop() + ($(window).height() / distance);
+			if(elemPos < enterPos) {
+				$(this).addClass(addClass);
+			}
+		});
+	};
+
+	// Check first
+	addAnimation('.drop-in','drop-in--visible');
+	addAnimation('.js-fade-in','fade--in');
+	addAnimation('.js-slide-left','slide--left');
+	addAnimation('.js-slide-right','slide--right');
+	addAnimation('.js-slide-in','slide--in', 1.25);
+    addAnimation('.js-draw-left','draw--left');
+    addAnimation('.js-draw-right','draw--right');
+
+	// Apply on scroll
+	$(window).scroll(function() {
+		addAnimation('.drop-in','drop-in--visible');
+		addAnimation('.js-fade-in','fade--in');
+        addAnimation('.js-fade-out','fade--out');
+		addAnimation('.js-slide-left','slide--left');
+		addAnimation('.js-slide-right','slide--right');
+		addAnimation('.js-slide-in','slide--in', 1.25);
+        addAnimation('.js-draw-left','draw--left');
+        addAnimation('.js-draw-right','draw--right');
+	});
+	
+	// Smooth scroll
+	$('.scrollto').click(function(event) {   
+	    event.preventDefault();
+	    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+	});
+
     $(window).scroll(function(){
 
         if ($(this).scrollTop() > 600 && !$('.navbar').hasClass('show-navbar') ) {
